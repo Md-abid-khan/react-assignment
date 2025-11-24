b0e053e7a6b5721c3b7f054034af2a3e46d09ced
Your NameAbid <abidkhanx25@gmail.com>
2025-11-24 17:02:10 +0600

    setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from "react-router/dom";
+import router from './Routes/Routes.jsx';
+
+// import { createBrowserRouter } from "react-router";
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello World</div>,
+//   },
+// ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
